ec2e7f02d51c4c0361194d28b8479dc94c8d36d0
J. Alexander <xandermoorman@gmail.com>
2017-11-17 22:45:42 -0600

    Github is stupid.

--- src/main/scala/com/vol/windows/SearchRange.scala
+++ src/main/scala/com/vol/windows/SearchRange.scala
@@ -1,51 +1,45 @@
 package com.bbs.vol.utils
 
-import java.net.InetAddress
-
-import com.google.common.net.InetAddresses
-
-import scala.util.Try
-
 trait SearchRange {
 
   /** Putting this off until I need it */
-  private[vol] def seachIpRange(value: String, start: String, end: String): Boolean = {
+  private[com] def searchIpRange(value: String, start: String, end: String): Boolean = {
     if(ipToLong(start) to ipToLong(end) contains ipToLong(value)) true
     else false
 
   } // END ipRange()
 
   /** Convert IP address to Long */
-  private[vol] def ipToLong(ip: String): Long = {
-    
-    /** Uses Google's Guava library to avoid DNS lookup costs */
-    val addr: InetAddress = InetAddresses.forString(ip)
-    // val addr = InetAddress.getByName(ip)
-    val ipAddr: Array[Byte] = addr.getAddress
-    var result: Long = 0L
-    for(value <- ipAddr){
-      result <<= 8
-      result |= value & 0xff
-    }
-    return result
+  private[com] def ipToLong(ip: String): Long = {
+
+    /** */
+    val splitIp = ip.split('.')
+    val octet1 = splitIp(0).toLong * 16777216 //cubed
+    val octet2 = splitIp(1).toLong * 65536
+    val octet3 = splitIp(2).toLong * 256
+    val octet4 = splitIp(3).toLong
+
+    val longValue = octet1 + octet2 + octet3 + octet4
+
+    return longValue
   } // END getLong
 
-  private[vol] def searchHexRange(value: String, start: String, end: String): Boolean = {
+  private[com] def searchHexRange(value: String, start: String, end: String): Boolean = {
     val bool = searchRange(hex2Long(value), hex2Long(start), hex2Long(end))
 
     return bool
   } // END hexRange()
-  private[vol] def searchHexRange(value: String, start: Long, end: Long): Boolean = {
+  private[com] def searchHexRange(value: String, start: Long, end: Long): Boolean = {
     val bool = start to end contains hex2Long(value)
     return bool
   }
-  private[vol] def searchHexRange(value: Long, start: Long, end: Long): Boolean = {
+  private[com] def searchHexRange(value: Long, start: Long, end: Long): Boolean = {
 
     val bool = searchRange(value, start, end)
 
-  return bool
+    return bool
   }
-  private[vol] def searchHexRange(value: Long, start: String, end: String): Boolean = {
+  private[com] def searchHexRange(value: Long, start: String, end: String): Boolean = {
 
     val bool = searchRange(value, hex2Long(start), hex2Long(end))
 
