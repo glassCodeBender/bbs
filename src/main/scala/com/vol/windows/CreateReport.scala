56c019a02fd2f30ebd32f28cc97317e32b267226
J. Alexander <xandermoorman@gmail.com>
2017-11-19 18:25:20 -0600

    Added a bit to the report.

--- src/main/scala/com/vol/windows/CreateReport.scala
+++ src/main/scala/com/vol/windows/CreateReport.scala
@@ -2,7 +2,7 @@ package com.bbs.vol.windows
 
 object CreateReport {
 
-  def run(memFile: String, os: String, process: ProcessBrain, disc: Discovery, riskRating: Int) = {
+  private[windows] def run(memFile: String, os: String, process: ProcessBrain, disc: Discovery, riskRating: Int) = {
     /** Using StringBuilder for fast concatenation of Strings. */
     val report = new StringBuilder()
 
@@ -77,9 +77,30 @@ object CreateReport {
     val vnc = vncCheck(net)
     if(vnc.nonEmpty) report.append(vnc)
 
+    /** \tMeterpreter DLL */
+    val meterpreter = checkMeterpreter(ldr)
+    if(meterpreter.nonEmpty)
+      report.append(meterpreter)
+
     /** \tMemory Leaks*/
+    val leaks = memoryLeaks(regPersist)
+    if(leaks.nonEmpty) report.append(leaks)
+
+    /** Run findHiddenExecs() and say if any execs were found. */
+
+
+
+
+
 
     /** Suspicious Console Commands */
+
+    if(sysSt.suspCmds.nonEmpty){
+      report.append("\n\tThe following potentially suspicious exams were found in the commandline info: \n\t")
+      report.append(sysSt.suspCmds.mkString("\n\t"))
+    }
+
+    /** Commandline History */
 
     /** PROCESS INFO */
 
@@ -91,6 +112,80 @@ object CreateReport {
     * **************Results Summary Section ************************
     * **************************************************************
     ****************************************************************/
+
+  /** Looks for hidden executables. */
+  private[this] def findHiddenExecs(vec: Vector[ProcessBbs]): Vector[String] = {
+
+    val hiddenExecPattern = {
+      Vector(".+//.xlsx.exe", ".+//.csv.exe", ".+//.doc.exe", ".+//.xls.exe", ".+//.xltx.exe", ".+//.xlt.exe",
+        ".+//.pdf.exe", ".+//.xlsb.exe", ".+//.xlsm.exe", ".+//.xlst.exe", ".+//.xml.exe", ".+//.txt.exe",
+        ".+//.ods.exe", ".+//.docx.exe", ".+//.dot.exe", ".+//.rtf.exe", ".+//.docm.exe", ".+//.dotm.exe",
+        ".+//.htm.exe", ".+//.mht.exe", ".+//.jpg.exe", ".+//.ppt.exe", ".+//.pptx.exe", ".+//.pot.exe",
+        ".+//.odp.exe", ".+//.ppsx.exe", ".+//.pps.exe", ".+//.pptm.exe", ".+//.potm.exe", ".+//.ppsm.exe",
+        ".+//.py.exe", ".+//.pl.exe", ".+//.eml.exe", ".+//.json.exe", ".+//.mp3.exe", ".+//.wav.exe", ".+//.aiff.exe",
+        ".+//.au.exe", ".+//.pcm.exe", ".+//.ape.exe", ".+//.wv.exe", ".+//.m4a.exe", ".+//.8svf.exe", ".+//.webm.exe",
+        ".+//.wv.exe", ".+//.wma.exe", ".+//.vox.exe", ".+//.tta.exe", ".+//.sln.exe", ".+//.raw.exe", ".+//.rm.exe",
+        ".+//.ra.exe", ".+//.opus.exe", ".+//.ogg.exe", ".+//.oga.exe", ".+//.mogg.exe", ".+//.msv.exe", ".+//.mpc.exe",
+        ".+//.mmf.exe", ".+//.m4b.exe", ".+//.ivs.exe", ".+//.ilkax.exe", ".+//.gsm.exe", ".+//.flac.exe",
+        ".+//.dvf.exe", ".+//.dss.exe", ".+//.dct.exe", ".+//.awb.exe", ".+//.amr.exe", ".+//.act.exe", ".+//.aax.exe",
+        ".+//.aa.exe", ".+//.3gp.exe", ".+//.webm.exe", ".+//.mkv.exe", ".+//.flv.exe", ".+//.vob.exe", ".+//.ogv.exe",
+        ".+//.ogg.exe", ".+//.gif.exe", ".+//.gifv.exe", ".+//.mng.exe", ".+//.avi.exe", ".+//.mov.exe", ".+//.qt.exe",
+        ".+//.wmv.exe", ".+//.yuv.exe", ".+//.rm.exe", ".+//.rmvb.exe", ".+//.asf.exe", ".+//.amv.exe", ".+//.mp4.exe",
+        ".+//.m4p.exe", ".+//.m4v.exe", ".+//.amv.exe", ".+//.asf.exe")
+    } // END hiddenExecPattern
+
+    /** Combine all the strings in the Vector to make a single regex */
+    val makeRegex = "(" + hiddenExecPattern.mkString("|") + ")"
+    val regex = makeRegex.r
+
+    /** Vector of process names. */
+    val procVec: Vector[String] = vec.map(x => x.name).distinct
+    val searchForHiddenProcs = procVec.map(x => regex.findFirstIn(x).getOrElse("None"))
+    val hiddenProcs = searchForHiddenProcs.filterNot(x => x.contains("None"))
+    if(hiddenProcs.nonEmpty) {
+      println("\nPrinting hidden executables.\n\n")
+      hiddenProcs.foreach(println)
+    }
+
+    return hiddenProcs
+  } // END hiddenExecPattern
+
+  private[this] def checkMeterpreter(vec: Vector[LdrInfo]) = {
+    var str = ""
+    val meter = vec.map(x => (x.pid, x.meterpreter))
+    val meterFound = meter.filter(x => x._2 == true)
+    if(meterFound.nonEmpty) {
+      str = "\n\tA DLL used by meterpreter was found on the system indicating that the system was breached."
+      val dllFound = for(value <- meterFound) yield s"\n\tA meterpreter DLL was found in PID: ${value._1}"
+      str = str + dllFound.mkString("\n")
+    }
+
+    str
+  } // END checkMeterpreter()
+
+  private[this] def memoryLeaks(vec: Vector[RegPersistenceInfo]) = {
+
+    var reportStr = ""
+    val regHandles: Vector[RegistryHandles] = vec.map(x => x.handles)
+
+    val count: Vector[(String, Int)] = regHandles.map(x => (x.pid, x.runCount))
+    val filterCount = count.filter(_._2 > 3)
+
+    var tempVec = Vector[String]()
+    if (filterCount.nonEmpty){
+      reportStr = "\n\tDuplicate run keys are an indication that an attacker used the registry to establish persistence.\n"
+      tempVec = for(values <- filterCount) yield s"\t${values._2} links to the run key were found in PID: ${values._1}"
+      reportStr = reportStr + tempVec.mkString("\n")
+      if(filterCount.exists(x => x._2 > 8)) {
+        reportStr = reportStr + {
+          s"\n\n\tWe have determined that an attacker used the run key to establish registry persistence.\n"
+        }
+      }
+    }
+
+    reportStr
+  } // END memoryLeaks()
+
   private[this] def vncCheck(vec: Vector[NetConnections]): String = {
     var str = ""
     val vncCheck = for{
@@ -195,7 +290,7 @@ object CreateReport {
     val docsTup = malDocs.map(x => (x.owner, x.rule))
     val docsVec =  for(value <- docsTup) yield "Process: " + value._1 + " Rule Found: " + value._2
     if(malDocs.nonEmpty) reportStr.append("\n\tExploit Kits:\n\t" + docsVec.mkString("\n\t"))
-    
+
     return reportStr.toString
   } // END malwareFound()
 } // END CreateReport
