b8c103851f7c6e6fb11d281c04240f2b12270f21
J. Alexander <xandermoorman@gmail.com>
2017-11-23 21:11:19 -0600

    Added code to clean up volatility directory when program complete

--- src/main/scala/com/vol/windows/FileFun.scala
+++ src/main/scala/com/vol/windows/FileFun.scala
@@ -1,11 +1,14 @@
 package com.bbs.vol.utils
 import java.io.{File, PrintWriter}
+import java.nio.file.StandardCopyOption.REPLACE_EXISTING
+import java.nio.file.{Files, Paths}
+
 import io.Source
 
 /** Interface for common file operations. */
 trait FileFun {
 
-  private[vol] def writeToFile(fileName: String, data: String) = {
+  private[vol] def writeToFile(fileName: String, data: String): Unit = {
     val writer = new PrintWriter(new File(fileName))
 
     /** Write CSV to a file. */
@@ -14,7 +17,7 @@ trait FileFun {
   } // END writeToFile
 
   /** Should probably make string generic. */
-  private[vol] def writeToFile(fileName: String, data: IndexedSeq[String]) = {
+  private[vol] def writeToFile(fileName: String, data: IndexedSeq[String]): Unit = {
 
     val writer = new PrintWriter(new File(fileName))
     val dataStr = data.mkString("\n")
@@ -36,14 +39,26 @@ trait FileFun {
     return read
   } // END readFileTransform
 
+  /** Move a sequence of files to a different location. */
+  private[vol] def cleanUp(files: Seq[String], dest: String) = {
+
+    for(file <- files) moveFile(file, dest)
+
+  }// END run()
+
+  /** Move a single file to a different directory. */
+  private[vol] def moveFile(file: String, dest: String) = {
+
+    Files.move(Paths.get(file), Paths.get(dest), REPLACE_EXISTING)
+
+  }// END run()
+
   /** Read a file and put the lines in a Vector */
   private[vol] def readFileToVec(fileName: String): Vector[String] = {
-
     val src = Source.fromFile( fileName )
-
     val read = src.getLines.toVector
-
     src.close
+
     return read
   } // END readFileToVec()
 
